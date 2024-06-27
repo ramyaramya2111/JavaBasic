c7d1ca36db7cd7394a14638b9d47b598f7973607
User <User@DESKTOP-UEBIJTF>
2024-06-27 11:58:33 +0530

    Day1--A project

---
+++ Javanew/src/day1/A.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class A {
+	public static void main(String args[])
+	{
+		
+	
+	char ch='a';
+	System.out.println(ch);
+	char ch1=65;
+	System.out.println(ch1);
+	char Var1='\u00A7';
+	System.out.println(Var1);
+	char Var2='\u20AC';
+	System.out.println(Var2);
+	int a='A';
+	System.out.println(a);
+	
+	}
+
+}
