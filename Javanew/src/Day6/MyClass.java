0720430948cc44a1711be792f1fc49280d2d4348
User <User@DESKTOP-UEBIJTF>
2024-07-10 21:11:52 +0530

    Day 6 Programs

---
+++ Javanew/src/Day6/MyClass.java
@@ -0,0 +1,27 @@
+package Day6;
+
+public class MyClass {
+private int section;
+private static int srNo;
+static
+{
+	System.out.println("----------------Within ststic Block---------");
+	srNo=1000;
+}
+MyClass()
+{
+	System.out.println("----------------Within Default Constructor-------------");
+	srNo++;
+	section++;
+}
+@Override
+public String toString() {
+	return "MyClass [Serial No"+srNo+",Section=" + section + "]";
+}
+static void display() 
+{
+//	System.out.println("Section : "+section); //can't access non static members	
+	System.out.println("Serial No. "+srNo); 
+}
+}
+
